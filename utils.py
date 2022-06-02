2f736950eec17049368f82568ceddb1a2288690f
eunsour <woo569628@naver.com>
2022-06-03 02:55:44 +0900

    update utils.py

--- utils.py
+++ utils.py
@@ -4,7 +4,7 @@ from config import log
 
 def load_data(path):
     data = []
-    with open(path, 'r', encoding='utf-8') as rf:
+    with open(path, 'r', encoding='utf-8-sig') as rf:
         for line in rf:
             line = line.strip()
             data.append(line)
@@ -14,10 +14,9 @@ def load_data(path):
 def split_data(data_list, ratio):
     """ ratio: 0 ~ 1
     """
-    random.seed(777)
-    random.shuffle(data_list) # shuffle
+    random.seed(42)
+    random.shuffle(data_list)
     pivot = int(len(data_list) * ratio)
     ldata = data_list[:pivot]
     rdata = data_list[pivot:]
     return ldata, rdata
-    
