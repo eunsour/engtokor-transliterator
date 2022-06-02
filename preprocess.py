64e405f0e26b194901ba6739fd703c7500a2006d
eunsour <woo569628@naver.com>
2022-06-03 02:55:54 +0900

    update preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -2,20 +2,29 @@ import pandas as pd
 
 from config import log
 
+
 def preprocessing(data):
-    log('> Preprocessing')
+    log("> Preprocessing")
 
-    data_lst = []
+    """
+    < Input Formatting >
+    ... ... ...
+    eng to kor  cable car  케이블 카
+    kor to eng  케이블 카   cable car
+    eng to kor  Toronto    토론토
+    kor to eng  토론토     Toronto
+    ... ... ...
+    """
     
+    preprocess_inputs = []
+
     for i, _ in enumerate(data):
-        source_eng = data[i].split('\t')[0]
-        target_kor = data[i].split('\t')[-1]
+        source_eng = data[i].split("\t")[0]
+        target_kor = data[i].split("\t")[-1]
 
-        data_lst.append(['eng to kor', source_eng, target_kor])
-        data_lst.append(["kor to eng", target_kor, source_eng])
+        preprocess_inputs.append(["eng to kor", source_eng, target_kor])
+        preprocess_inputs.append(["kor to eng", target_kor, source_eng])
 
-    df = pd.DataFrame(
-        data_lst, columns=["prefix", "input_text", "target_text"]
-    )
+    df = pd.DataFrame(preprocess_inputs, columns=["prefix", "input_text", "target_text"])
 
     return df
