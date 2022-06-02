5ca34927fb97aba709dddf9f99045530635ae0b9
eunsour <woo569628@naver.com>
2022-06-02 19:44:25 +0900

    add utils.py

---
+++ utils.py
@@ -0,0 +1,23 @@
+import random
+
+from config import log
+
+def load_data(path):
+    data = []
+    with open(path, 'r', encoding='utf-8') as rf:
+        for line in rf:
+            line = line.strip()
+            data.append(line)
+    return data
+
+
+def split_data(data_list, ratio):
+    """ ratio: 0 ~ 1
+    """
+    random.seed(777)
+    random.shuffle(data_list) # shuffle
+    pivot = int(len(data_list) * ratio)
+    ldata = data_list[:pivot]
+    rdata = data_list[pivot:]
+    return ldata, rdata
+    
