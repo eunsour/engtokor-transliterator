12ad341c0f30b1594eb93e5f58825f2ccb20f3ba
eunsour <woo569628@naver.com>
2022-06-02 19:44:02 +0900

    update transliteration.py

--- transliteration.py
+++ transliteration.py
@@ -8,7 +8,10 @@ warnings.filterwarnings("ignore")
 from sklearn.model_selection import train_test_split
 from simpletransformers.t5 import T5Model, T5Args
 
+from utils import *
 from config import *
+from preprocess import *
+from model import Seq2seqAtt
 
 parser = argparse.ArgumentParser(description="")
 parser.add_argument("--train", action="store_true", help="Train Mode")
@@ -28,36 +31,26 @@ pretrained_model_path = "./outputs/checkpoint-4168-epoch-4"
 class Transliterator(object):
     def __init__(self):
         self._load_data()
+        self._process_data()
+
 
     def _load_data(self):
-        train_lst, eval_lst = [], []
-        header = ["prefix", "input_text", "target_text"]
+        raw = load_data(data_path)
+        train, eval = split_data(raw, ratio=params['TRAIN_RATIO'])
 
-        data = pd.read_csv(data_path, sep="\t", encoding="utf-8-sig")
-        data["prefix"] = "eng to kor"
+        log(">> total number of data:", len(train) + len(eval))
+        log(">> number of train data:", len(train))
+        log(">> number of test data:", len(eval))
 
-        train, eval = train_test_split(data, test_size=params['VALID_RATIO'], random_state=42)
+        self.train = train
+        self.eval = eval
+    
+    def _process_data(self):
+        self.train_df = preprocessing(self.train)
+        self.eval_df = preprocessing(self.eval)
 
-        train_to_lst = train[header].values.tolist()
-        eval_to_lst = eval[header].values.tolist()
-
-        for data in train_to_lst:
-            train_lst.append([data[0], data[1], data[2]])
-            train_lst.append(["kor to eng", data[2], data[1]])
-
-        for data in eval_to_lst:
-            eval_lst.append([data[0], data[1], data[2]])
-            eval_lst.append(["kor to eng", data[2], data[1]])
-
-        train_df = pd.DataFrame(
-            train_lst, columns=["prefix", "input_text", "target_text"]
-        )
-        eval_df = pd.DataFrame(
-            eval_lst, columns=["prefix", "input_text", "target_text"]
-        )
-
-        self.train_df = train_df
-        self.eval_df = eval_df
+        self.seq2seq_att = Seq2seqAtt(params)
+        self.seq2seq_att.build_model()
         
         
     def run_train(self):
@@ -65,27 +58,7 @@ class Transliterator(object):
 
         self.train_df["prefix"], self.eval_df["prefix"] = "", ""
 
-        model_args = T5Args()
-        model_args.max_seq_length = params["MAX_SEQUENCE_LENGTH"]
-        model_args.train_batch_size = params["BATCH_SIZE"]
-        model_args.eval_batch_size = params["BATCH_SIZE"]
-        model_args.num_train_epochs = params["EPOCHS"]
-        model_args.evaluate_during_training = True
-        model_args.evaluate_during_training_steps = 30000
-        model_args.use_multiprocessing = False
-        model_args.fp16 = False
-        model_args.save_steps = -1
-        model_args.save_eval_checkpoints = False
-        model_args.no_cache = True
-        model_args.reprocess_input_data = True
-        model_args.overwrite_output_dir = True
-        model_args.preprocess_inputs = False
-        model_args.num_return_sequences = 1
-        model_args.best_model_dir = f"outputs-{SAVE_NAME}-/best_model"
-        model_args.output_dir = f"outputs-{SAVE_NAME}-/"
-        model_args.wandb_project = "eng2kor transliterator using mT5-base"
-
-        self.model = T5Model("mt5", "google/mt5-base", use_cuda=True, args=model_args)
+        self.model =  self.seq2seq_att.model
         self.model.train_model(self.train_df, eval_data=self.eval_df)
 
 
@@ -94,7 +67,7 @@ class Transliterator(object):
         model_args.max_seq_length = params["MAX_SEQUENCE_LENGTH"]
 
         self.model = T5Model(
-            "mt5", pretrained_model_path, use_cuda=True, args=model_args
+            "mt5", pretrained_model_path, use_cuda=False, args=model_args
         )
 
 
